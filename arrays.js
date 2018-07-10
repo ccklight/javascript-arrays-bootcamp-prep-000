08b4917856576caeeea4eb8db5e6c6d6af8533cc
Cheryl Kelson <cherylkelson@icloud.com>
2018-07-10 15:56:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,14 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']; 
- function addElementToBeginningOfArray (array, element) {
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  array.unshift("array");
+  array.unshift("element");
   return [element,...array];
   }
- function destructivelyAddElementToBeginninfOfArray (array, element){
-   array.unshift(element);
-   return array;
+ function destructivelyAddElementToBeginninfOfArray (array, element) {
+   array.unshift("array");
+   array.unshift("element");
+   return [element,...array];
    
  function addElementToEndfArray(array, element){
    return [...array, element]}
